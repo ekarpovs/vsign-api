51cd364f7824ea29cf59c30a3edf3136ecfd450d
Evgeny Karpovsky <ekarpovsky@gmail.com>
2019-05-20 18:52:53 +0300

    Cosmetic changes

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response, RequestHandler } from 'express';
-import CompanySchema from '../models/company.model';
+import companySchemaModel from '../models/company.model';
+import { Result } from 'range-parser';
 
-const cs = CompanySchema;
+const csm = companySchemaModel;
 
 export const list: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const companies = await cs.find({});
+    const companies = await csm.find({});
     return res.status(200).json(companies);
   } catch ( error ) {
     return res.status(500).json(error);
@@ -13,9 +14,9 @@ export const list: RequestHandler = async (req: Request, res: Response, next: Ne
 };
 
 export const create: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+  const newCsm = new csm(req.body);
   try {
-    const newCompany = new cs(req.body);
-    const company = await newCompany.save();
+    const company = await newCsm.save();
     return res.status(200).json(company);
   } catch ( error ) {
     return res.status(500).json(error);
--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction, Application } from 'express';
-import * as companyController from '../controllers/company.controller';
+import * as cc from '../controllers/company.controller';
 
 export const register = ( app: Application ) => {
   app.route('/api/company')
-  .get(companyController.list)
-  .post(companyController.create);
+  .get(cc.list)
+  .post(cc.create);
 };
