fc2dc3d36ce1a27c3143c47cc2be4800b59dbfa6
Evgeny Karpovsky <ekarpovsky@gmail.com>
2019-09-20 06:59:52 +0300

    Use Atlas instead local DB, fix domains route

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ import * as express from './configuration/express.config';
 // initialize configuration
 dotenv.config();
 
+const dbUrl = process.env.NODE_ENV === 'prod' ?
+    process.env.MONGODB_URI_PROD :
+    process.env.MONGODB_URI_LOCAL;
+
 // Connect to MongoDb
 const dbOptions = {
-    url: 'mongodb://localhost/vsadmindb',
+    url: dbUrl,
     useCreateIndex: true,
     useNewUrlParser: true
 };
--- src/routes/domain.routes.ts
+++ src/routes/domain.routes.ts
@@ -3,11 +3,11 @@ import * as cc from '../controllers/domain.controller';
 import { checkIfAuthenticated } from '../services/auth.service';
 
 export const register = ( app: Application ) => {
-  app.route('/api/domain')
+  app.route('/api/domains')
   .get(cc.list)
   .post(cc.create);
 
-  app.route('/api/domain/:id')
+  app.route('/api/domains/:id')
   .get(checkIfAuthenticated, cc.one)
   .put(checkIfAuthenticated, cc.update)
   .delete(checkIfAuthenticated, cc.lock);
