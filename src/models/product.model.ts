4dfc0f073f742173336488133e876808aabcae40
Evgeny Karpovsky <ekarpovsky@gmail.com>
2019-10-17 17:42:32 +0300

    Improve product mode

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,5 +1,11 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { IDomainModel } from './domain.model';
+import { IUserModel } from './user.model';
+
+export interface IRegisteredModel extends Document {
+  by: IUserModel;
+  date: Date;
+}
 
 export interface IProductModel extends Document {
   domain: IDomainModel;
@@ -7,14 +13,21 @@ export interface IProductModel extends Document {
   description: string;
   locked: boolean;
   name: string;
+  registered: IRegisteredModel;
 }
+
+const RegisteredSchema: Schema = new Schema({
+  by: { type: Schema.Types.ObjectId, ref: 'User', unique: false},
+  date: { type: Date, default: Date.now() }
+}, { _id: false });
 
 const ProductSchema: Schema = new Schema({
   created: { type: Date, default: Date.now() },
   description: String,
   domain: { type: Schema.Types.ObjectId, ref: 'Domain', unique: false },
   locked: Boolean,
-  name: { type: String, required: true, unique: false }
+  name: { type: String, required: true, unique: false },
+  registered: RegisteredSchema
 });
 ProductSchema.index({ name: 1, domain: 1 }, { unique: true });
 
